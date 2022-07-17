9058f8f8e7864e02544df6a1b587d086c0520ff0
allanstar-byte <allanmutisya263@gmail.com>
2022-07-17 18:48:03 +0300

    site

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About(props) {
+    return ( <
+        aside >
+        <
+        img src = { props.image } >
+
+        <
+        /aside>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import blogData from "../data/blog";
+
+function Header(props) {
+    return ( <
+        header >
+        <
+        h1 > { props.name } < /h1> 
+
+        <
+        /
+        header >
+    );
+};
+
+export default Header;
